c847b8ed3a28fbeb3a5d6846a6f3ed220b042b3b
modianolubis <mhdmodiano@gmail.com>
2017-08-20 17:05:54 +0700

    Aplikasi prediksi usia nikah

---
+++ index.js
@@ -0,0 +1,63 @@
+var usia =prompt("SILAHKAN MASUKKAN USIA NIKAH LO MBLO!!");
+var usiaInt = parseInt(usia);
+var AnakPertamaLahirUmurLo, AnakMasukSDUmurLo, AnakMasukSLTPUmurLo, AnakMasukSLTAUmurLo, AnakMasukKuliahUmurLo, AnakLulusKuliahUmurLo, AnakMenikahUmurLo; 
+if (usia < 20) {
+  console.log("LO TERLALU DINI UNTUK MENIKAH")
+  AnakPertamaLahirUmurLo = usiaInt + 2;
+  AnakMasukSDUmurLo = AnakPertamaLahirUmurLo + 7;
+  AnakMasukSLTPUmurLo = AnakMasukSDUmurLo  + 6;
+  AnakMasukSLTAUmurLo = AnakMasukSLTPUmurLo + 3;
+  AnakMasukKuliahUmurLo = AnakMasukSLTAUmurLo + 3;
+  AnakLulusKuliahUmurLo = AnakMasukKuliahUmurLo + 4;
+  AnakMenikahUmurLo = AnakLulusKuliahUmurLo + 2;
+  alert("LO TERLALU DINI UNTUK MENIKAH \n Anak Pertama Lahir Umur Lo: " + AnakPertamaLahirUmurLo + "\n Anak Masuk SD Umur Lo: " + AnakMasukSDUmurLo + "\n Anak Masuk SLTP Umur Lo: " + AnakMasukSLTPUmurLo + "\n Anak Masuk SLTA Umur Lo: " + AnakMasukSLTAUmurLo + "\n Anak Masuk Kuliah Umur Lo: " + AnakMasukKuliahUmurLo + "\n Anak Lulus Kuliah Umur Lo: " + AnakLulusKuliahUmurLo + "\n Anak Menikah Umur Lo: " + AnakMenikahUmurLo);
+  alert("NASEHAT : \n \n MAPAN LAHIR DAN BATIN DULU MBLO!! \n \n <<THANK YOU SUDAH MAMPIR>>")
+  
+} else if (usia >=20 && usia <=28) {
+  console.log("KAMU IDEAL UNTUK MENIKAH");
+  AnakPertamaLahirUmurLo = usiaInt + 2;
+  AnakMasukSDUmurLo = AnakPertamaLahirUmurLo + 7;
+  AnakMasukSLTPUmurLo = AnakMasukSDUmurLo  + 6;
+  AnakMasukSLTAUmurLo = AnakMasukSLTPUmurLo + 3;
+  AnakMasukKuliahUmurLo = AnakMasukSLTAUmurLo + 3;
+  AnakLulusKuliahUmurLo = AnakMasukKuliahUmurLo + 4;
+  AnakMenikahUmurLo = AnakLulusKuliahUmurLo + 2;
+  alert("KAMU IDEAL UNTUK MENIKAH \n Anak Pertama Lahir Umur Lo: " + AnakPertamaLahirUmurLo + "\n Anak Masuk SD Umur Lo: " + AnakMasukSDUmurLo + "\n Anak Masuk SLTP Umur Lo: " + AnakMasukSLTPUmurLo + "\n Anak Masuk SLTA Umur Lo: " + AnakMasukSLTAUmurLo + "\n Anak Masuk Kuliah Umur Lo: " + AnakMasukKuliahUmurLo + "\n Anak Lulus Kuliah Umur Lo: " + AnakLulusKuliahUmurLo + "\n Anak Menikah Umur Lo: " + AnakMenikahUmurLo);
+  alert("NASEHAT : \n \n TUNGGU APALAGI?? HALALKAN SEKARANG ATAU TINGGALKAN MBLO!! \n \n <<THANK YOU SUDAH MAMPIR>>")
+
+} else if (usia >=29 && usia <=31) {
+  console.log("LO SUDAH MATANG UNTUK MENIKAH");
+  AnakPertamaLahirUmurLo = usiaInt + 2;
+  AnakMasukSDUmurLo = AnakPertamaLahirUmurLo + 7;
+  AnakMasukSLTPUmurLo = AnakMasukSDUmurLo  + 6;
+  AnakMasukSLTAUmurLo = AnakMasukSLTPUmurLo + 3;
+  AnakMasukKuliahUmurLo = AnakMasukSLTAUmurLo + 3;
+  AnakLulusKuliahUmurLo = AnakMasukKuliahUmurLo + 4;
+  AnakMenikahUmurLo = AnakLulusKuliahUmurLo + 2;
+  alert("LO SUDAH MATANG UNTUK MENIKAH \n Anak Pertama Lahir Umur Lo: " + AnakPertamaLahirUmurLo + "\n Anak Masuk SD Umur Lo: " + AnakMasukSDUmurLo + "\n Anak Masuk SLTP Umur Lo: " + AnakMasukSLTPUmurLo + "\n Anak Masuk SLTA Umur Lo: " + AnakMasukSLTAUmurLo + "\n Anak Masuk Kuliah Umur Lo: " + AnakMasukKuliahUmurLo + "\n Anak Lulus Kuliah Umur Lo: " + AnakLulusKuliahUmurLo + "\n Anak Menikah Umur Lo: " + AnakMenikahUmurLo);
+  alert("NASEHAT : \n \n BURUAN!! HALALKAN SEKARANG JANGAN BANYAK PILIH!! \n \n <<THANK YOU SUDAH MAMPIR>>")
+  
+} else if (usia >=32 && usia <=34) {
+  console.log("STATUS LO WASPADA UNTUK MENIKAH");
+  AnakPertamaLahirUmurLo = usiaInt + 2;
+  AnakMasukSDUmurLo = AnakPertamaLahirUmurLo + 7;
+  AnakMasukSLTPUmurLo = AnakMasukSDUmurLo  + 6;
+  AnakMasukSLTAUmurLo = AnakMasukSLTPUmurLo + 3;
+  AnakMasukKuliahUmurLo = AnakMasukSLTAUmurLo + 3;
+  AnakLulusKuliahUmurLo = AnakMasukKuliahUmurLo + 4;
+  AnakMenikahUmurLo = AnakLulusKuliahUmurLo + 2;
+  alert("STATUS KAMU WASPADA UNTUK MENIKAH \n Anak Pertama Lahir Umur Lo: " + AnakPertamaLahirUmurLo + "\n Anak Masuk SD Umur Lo: " + AnakMasukSDUmurLo + "\n Anak Masuk SLTP Umur Lo: " + AnakMasukSLTPUmurLo + "\n Anak Masuk SLTA Umur Lo: " + AnakMasukSLTAUmurLo + "\n Anak Masuk Kuliah Umur Lo: " + AnakMasukKuliahUmurLo + "\n Anak Lulus Kuliah Umur Lo: " + AnakLulusKuliahUmurLo + "\n Anak Menikah Umur Lo: " + AnakMenikahUmurLo);
+  alert("NASEHAT : \n \n APALAGI YANG LO TUNGGU?? JANGAN TUNDA LAGI!! \n \n <<THANK YOU SUDAH MAMPIR>>")
+  
+} else if (usia >=35 && usia <=100) {
+  console.log("STATUS LO SIAGA UNTUK MENIKAH");
+  AnakPertamaLahirUmurLo = usiaInt + 2;
+  AnakMasukSDUmurLo = AnakPertamaLahirUmurLo + 7;
+  AnakMasukSLTPUmurLo = AnakMasukSDUmurLo  + 6;
+  AnakMasukSLTAUmurLo = AnakMasukSLTPUmurLo + 3;
+  AnakMasukKuliahUmurLo = AnakMasukSLTAUmurLo + 3;
+  AnakLulusKuliahUmurLo = AnakMasukKuliahUmurLo + 4;
+  AnakMenikahUmurLo = AnakLulusKuliahUmurLo + 2;
+  alert("STATUS LO SIAGA UNTUK MENIKAH \n Anak Pertama Lahir Umur Lo: " + AnakPertamaLahirUmurLo + "\n Anak Masuk SD Umur Lo: " + AnakMasukSDUmurLo + "\n Anak Masuk SLTP Umur Lo: " + AnakMasukSLTPUmurLo + "\n Anak Masuk SLTA Umur Lo: " + AnakMasukSLTAUmurLo + "\n Anak Masuk Kuliah Umur Lo: " + AnakMasukKuliahUmurLo + "\n Anak Lulus Kuliah Umur Lo: " + AnakLulusKuliahUmurLo + "\n Anak Menikah Umur Lo: " + AnakMenikahUmurLo);
+  alert("NASEHAT : \n \n APALAGI YANG LO TUNGGU?? SEKARANGLAH MENIKAH ATAU TIDAK SELAMANYA!! \n \n <<THANK YOU SUDAH MAMPIR>>")
+}
